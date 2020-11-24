ff10469f7d11b2da70ae418d7dd06c287618d35e
mozumderTushar <itztushar55@gmail.com>
2020-11-24 18:54:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { createStore } = require("redux");
+
+const initialState = {
+    age:21
+}
+
+const myReducer = (state = initialState, action) =>{
+    const newState = {...state}
+
+    if(action.type === 'ADD'){
+        newState.age += 1;
+    }
+    if(action.type === 'SUBTRACT'){
+        newState.age -= 1;
+    }
+
+    return newState;
+}
+
+const store = createStore(myReducer);
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({type: 'ADD'});
+store.dispatch({type: 'SUBTRACT'});
